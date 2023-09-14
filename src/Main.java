5edf75d1d28fb4c33315239ce822c1e0360c7786
colemaih <colemaih@mail.uc.edu>
2023-09-14 15:47:53 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int intKids= 5;
+        String rain = "no";
+        double doubleGallonOfGas= 3.85;
+        int intFavNumber=5;
+        double doubleShoeSize= 11.5;
+        int intBirthMonth = 6;
+        String fullName = "Isaiah Coleman";
+        System.out.println("My full name is " +fullName);
+    }
+}
